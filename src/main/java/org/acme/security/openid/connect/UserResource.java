67c353acbfa124b9e88c86bc86733795a52c0bae
cfaife <clerio.faife@gmail.com>
2021-11-22 08:43:03 +0200

    initial commit

---
+++ src/main/java/org/acme/security/openid/connect/UserResource.java
@@ -0,0 +1,38 @@
+package org.acme.security.openid.connect;
+
+import io.quarkus.security.identity.SecurityIdentity;
+import org.jboss.resteasy.annotations.cache.NoCache;
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/api/users")
+public class UserResource {
+
+    @Inject
+    SecurityIdentity securityIdentity;
+
+    @GET
+    @Path("/me")
+    @RolesAllowed("user")
+    @NoCache
+    public User me(){
+        return  new User(securityIdentity);
+    }
+
+    public static class User{
+        private final String userName;
+
+        User(SecurityIdentity securityIdentity){
+            this.userName = securityIdentity.getPrincipal().getName();
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+    }
+
+
+
+}
